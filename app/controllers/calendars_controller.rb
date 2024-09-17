42fd900b165739a725c3b9e12b995c21f4ddec57
youri0416 <withuuuu0416@gmail.com>
2024-09-17 19:21:45 +0900

    p

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,6 @@ class CalendarsController < ApplicationController
       end
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
-
       @week_days.push(days)
     end
 
